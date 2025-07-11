eb6e1ca9e6bd53e5c96b36b4c77060ef560679f5
Kirill Safarchev <83013244+klirii@users.noreply.github.com>
2025-07-11 15:55:09 +0400

    Update DllMain.cpp

--- DllMain.cpp
+++ DllMain.cpp
@@ -10,87 +10,87 @@ jobject thePlayer;
 jobject capabilities;
 
 /*
-* Все названия классов, функций (методов) и переменных (полей) майнкрафта нужно указывать в зашифрованном виде, т.к они зашифрованы.
-* Проверить это легко - достаточно открыть джарник игры через JD-GUI.
+* All names of classes, methods and fields of minecraft should be specified in encrypted form, because they are encrypted.
+* For clarity you can open the game jar file in the program JD-GUI (https://java-decompiler.github.io/).
 */
 
-void getEnv() { // Функция для получения указателя на JNIEnv
+void getEnv() { // Function to get a pointer to JNIEnv
 	JavaVM* jvm;
 
-	typedef jint(*pJNI_GetCreatedJavaVMs)(JavaVM** vmBuf, jsize bufLen, jsize* nVMs); // Создаём прототип функции JNI_GetCreatedJavaVMs
+	typedef jint(*pJNI_GetCreatedJavaVMs)(JavaVM** vmBuf, jsize bufLen, jsize* nVMs); // Create a prototype of the JNI_GetCreatedJavaVMs function
 	pJNI_GetCreatedJavaVMs JNI_GetCreatedJavaVMs = reinterpret_cast<pJNI_GetCreatedJavaVMs>(GetProcAddress(GetModuleHandleA("jvm.dll"), "JNI_GetCreatedJavaVMs"));
-	// Присваиваем прототипу этой функции адрес этой функции в игре
+	// Assign the address of this function in the game to the prototype
 
-	JNI_GetCreatedJavaVMs(&jvm, 1, NULL); // Вызываем эту функцию
+	JNI_GetCreatedJavaVMs(&jvm, 1, NULL); // Call this function
 	cout << "[DEBUG] JVM*: " << (LPVOID)jvm << endl;
-	jvm->AttachCurrentThread(reinterpret_cast<void**>(&env), NULL); // Получаем указатель на JNIEnv
+	jvm->AttachCurrentThread(reinterpret_cast<void**>(&env), NULL); // Get a pointer to JNIEnv
 }
 
-jobject getMC() { // Функция для получения объекта класса Minecraft (theMinecraft)
-	jclass Minecraft = env->FindClass("ave"); // Ищем класс "Minecraft" в зашифрованном виде
+jobject getMC() { // Function to get an object of the Minecraft class (theMinecraft)
+	jclass Minecraft = env->FindClass("ave"); // Looking for the class вЂњMinecraftвЂќ in encrypted form
 	cout << "[DEBUG] Minecraft: " << Minecraft << endl;
-	jmethodID smid = env->GetStaticMethodID(Minecraft, "A", "()Lave;"); // Получаем адрес функции (метода), которая возвращает объект класса Minecraft
+	jmethodID smid = env->GetStaticMethodID(Minecraft, "A", "()Lave;"); // Get the handle of the method that returns an object of the Minecraft class
 	cout << "[DEBUG] public static Minecraft getMinecraft(): " << smid << endl;
-	return env->CallStaticObjectMethod(Minecraft, smid); // Вызываем функцию getMinecraft() из игры, и возвращаем в качестве результата нашей функции результат getMinecraft()
+	return env->CallStaticObjectMethod(Minecraft, smid); // Call the getMinecraft() method from the game, and return the result of our function as the result of getMinecraft()
 }
 
 /*
-* 3-й аргумент функции GetStaticMethodID() - подпись метода, где
-* () - указывают на то, что это функция (то, что идёт после скобочек - тип возвращаемого значения функции)
-* L - нужен, если функция возвращает объект класса, этот символ обозначает то, что после него должно идти полное название класса, объект которого мы возвращаем
-* Полное название класса - название класса вместе с пакетом
-* Например: Lnet/minecraft/client/Minecraft;
-* ; - обозначает окончание полного названия класса
-* Подробнее - https://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/types.html
+* The 3rd argument of the GetStaticMethodID() is the method signature, where:
+* () - indicate that this is a method (what comes after the brackets is the type of the method's return value)
+* L - is needed if the method returns an object, this symbol indicates that it should be followed by the full name of the class whose object we return
+* Full class name = the name of the class along with the package
+* For example: Lnet/minecraft/client/Minecraft;
+* ; - indicates the ending of the full name of the class
+* Read more - https://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/types.html
 */
 
-jobject getPlayer() { // Функция для получения объекта класса EntityPlayerSP (thePlayer)
-	jfieldID fid = env->GetFieldID(env->GetObjectClass(mc), "h", "Lbew;"); // Получаем адрес объекта thePlayer (адрес поля в классе Minecraft)
+jobject getPlayer() { // Function to get an object of EntityPlayerSP class (thePlayer)
+	jfieldID fid = env->GetFieldID(env->GetObjectClass(mc), "h", "Lbew;"); // Get the handle of thePlayer object
 	cout << "[DEBUG] public EntityPlayerSP thePlayer: " << fid << endl;
-	return env->GetObjectField(mc, fid); // Получаем сам объект, и возвращаем его в качестве результата функции
+	return env->GetObjectField(mc, fid); // Get the object and return it as the result of the function
 }
 
-jobject getCapabilities() { // Функция для получения объекта класса EntityPlayer (capabilities)
-	jclass EntityPlayer = env->FindClass("wn"); // Ищем класс EntityPlayer в зашифрованном виде
+jobject getCapabilities() { // Function to get an object of EntityPlayer class (capabilities)
+	jclass EntityPlayer = env->FindClass("wn"); // Looking for EntityPlayer class in encrypted form
 	cout << "[DEBUG] EntityPlayer: " << EntityPlayer << endl;
-	jfieldID fid = env->GetFieldID(EntityPlayer, "bA", "Lwl;"); // Получаем адрес объекта capabilities (адрес поля в классе EntityPlayer)
+	jfieldID fid = env->GetFieldID(EntityPlayer, "bA", "Lwl;"); // Get the handle of the capabilities object
 	cout << "[DEBUG] public PlayerCapabilities capabilities: " << fid << endl;
-	return env->GetObjectField(thePlayer, fid); // Получаем сам объект capabilities
+	return env->GetObjectField(thePlayer, fid); // Get an object and return it
 }
 
-void initObjects() { // Функция для инициализации объектов, через которые мы будем работать с полями и методами игры
+void initObjects() { // Function for initialization of objects through which we will work with the fields and methods of the game
 	mc = getMC();
 	thePlayer = getPlayer();
 	capabilities = getCapabilities();
 }
 
 void Fly() {
-	jfieldID fid = env->GetFieldID(env->GetObjectClass(capabilities), "c", "Z"); // Получаем адрес поля allowFlying
+	jfieldID fid = env->GetFieldID(env->GetObjectClass(capabilities), "c", "Z"); // Get the handle of the allowFlying field
 	cout << "[DEBUG] public boolean allowFlying: " << fid << endl;
-	env->SetBooleanField(capabilities, fid, true); // Устанавливаем переменную allowFlying на true
+	env->SetBooleanField(capabilities, fid, true); // Set the allowFlying field to true
 }
 
 float getFlySpeed() {
-	jmethodID mid = env->GetMethodID(env->GetObjectClass(capabilities), "a", "()F"); // Получаем адрес метода getFlySpeed
-	return env->CallFloatMethod(capabilities, mid); // Вызываем функцию и получаем скорость флая
+	jmethodID mid = env->GetMethodID(env->GetObjectClass(capabilities), "a", "()F"); // Get the handle of getFlySpeed method
+	return env->CallFloatMethod(capabilities, mid); // Call the method and get the fly speed
 }
 
 void setFlySpeed(float speed) {
-	jmethodID mid = env->GetMethodID(env->GetObjectClass(capabilities), "a", "(F)V"); // Получаем адрес метода setFlySpeed
-	env->CallVoidMethod(capabilities, mid, 0.05 * speed); // Вызываем его и устанавливаем скорость флая
+	jmethodID mid = env->GetMethodID(env->GetObjectClass(capabilities), "a", "(F)V"); // Get the handle of the setFlySpeed method
+	env->CallVoidMethod(capabilities, mid, 0.05 * speed); // Call it and set the fly speed.
 }
 
 BOOL WINAPI DllMain(HINSTANCE handle, DWORD reason, LPVOID reserved) {
 	switch (reason) {
 	case DLL_PROCESS_ATTACH:
-		AllocConsole(); // Выделяем консоль
-		freopen("CONOUT$", "w", stdout); // Выделяем поток stdout для консоли
+		AllocConsole(); // Allocate console
+		freopen("CONOUT$", "w", stdout); // Allocate the stdout stream for the console
 
-		getEnv(); // Получаем JNIEnv*
+		getEnv(); // Get JNIEnv*
 		cout << "[DEBUG] JNIEnv*: " << (LPVOID)env << endl;
-		initObjects(); // Инициализируем объекты для работы
+		initObjects(); // Initialize objects for work
 
-		Fly(); // Делаем fly
+		Fly(); // Make fly
 		setFlySpeed(2);
 		cout << "[DEBUG] Current fly speed: " << getFlySpeed() << endl;
 	}
